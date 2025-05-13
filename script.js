feb43d2426fb62c6244af37737df9e1f90f98206
Lorena Morais <lorenapachecomorais@gmail.com>
2025-05-12 21:49:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,9 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Botão não encontrado");
   }
 });
+
+// script.js
+document.getElementById('theme-toggle').addEventListener('click', function() {
+    // Alterna a classe dark-theme no body
+    document.body.classList.toggle('dark-theme');
+});
